db1abaa1bcb8f46422d69fda8e739a24ebe76bcf
cakhanif <nichazah@gmail.com>
2015-12-29 09:29:56 +0700

    Pemilukada 2015

---
+++ Sosialisasi_Pemilukada/Forms/sosialisasi_form.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sosialisasi_Pemilukada.Forms
+{
+    public partial class sosialisasi_form : Form
+    {
+        public sosialisasi_form()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void btnBack_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            main_form main = new main_form();
+            main.Show();
+        }
+
+        private void btnBack_MouseHover(object sender, EventArgs e)
+        {
+            btnBack.BackgroundImage = global::Sosialisasi_Pemilukada.Properties.Resources.BtnBack_Hover;
+        }
+
+        private void btnBack_MouseLeave(object sender, EventArgs e)
+        {
+            btnBack.BackgroundImage = global::Sosialisasi_Pemilukada.Properties.Resources.BtnBack;
+        }
+    }
+}
---
+++ Sosialisasi_Pemilukada/Source/gambar_load.cs
@@ -0,0 +1,42 @@
+﻿using Sosialisasi_Pemilukada.Database;
+using System.Data.SQLite;
+using System.Drawing;
+using System.Data;
+using System.IO;
+
+namespace Sosialisasi_Pemilukada.Source
+{
+    class gambar_load
+    {
+        private db_connect connect;
+        private string _query;
+
+        public Image gambar { get; set; }
+
+        public gambar_load()
+        {
+            _query = "";
+            connect = new db_connect("database.db");
+        }
+
+        public void ShowImage(int x)
+        {
+            connect.Open();
+
+            _query = "select gambar from candidates where id = '" + x + "'";
+            var command = new SQLiteCommand(_query, connect.Connect);
+            command.ExecuteNonQuery();
+
+            SQLiteDataAdapter data = new SQLiteDataAdapter(command);
+            DataTable da = new DataTable();
+
+            data.Fill(da);
+            byte[] img = (byte[])da.Rows[0][0];
+
+            MemoryStream convert = new MemoryStream(img);
+            gambar = Image.FromStream(convert);
+
+            connect.Close();
+        }
+    }
+}
